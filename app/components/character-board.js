36e3061c646b3c8b43a7ce58af70be8a366dc8bd
Ramiro Jr. Franco <rjfranco@gmail.com>
2016-03-01 02:00:56 -0800

    Adding some functionality around answer checking, and related styles.

--- app/components/character-board.js
+++ app/components/character-board.js
@@ -2,5 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['character-board', 'hiragana'],
-  tagName: 'section'
+  tagName: 'section',
+
+  actions: {
+    checkInput: function() {
+      this.set('has_checked', true);
+      this.set('answer_correct', this.get('user_input').toLowerCase() === this.get('expected_input'));
+    }
+  },
+
+  formStatus: function() {
+    if(this.get('has_checked')) {
+      if(this.get('answer_correct')) {
+        return 'answer-correct';
+      } else {
+        return 'answer-error';
+      }
+    }
+  }.property('has_checked', 'answer_correct')
 });
