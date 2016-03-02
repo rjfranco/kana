f315f8207f483262564cad91e02847aa115de0a8
Ramiro Jr. Franco <rjfranco@gmail.com>
2016-03-01 17:28:54 -0800

    Correcting route to conditionally use JSON parser when run in electron.

--- app/routes/hiragana.js
+++ app/routes/hiragana.js
@@ -4,7 +4,8 @@ export default Ember.Route.extend({
   model: function() {
     return $.ajax('syllabaries.json')
     .then(function(data) {
-      return this.store.push(JSON.parse(data));
+      let kana = window.ELECTRON ? JSON.parse(data) : data;
+      return this.store.push(kana);
     }.bind(this));
   }
 });
