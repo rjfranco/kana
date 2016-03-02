632d48525b9b64ffdc3bf1a24ae9a252c3467921
Ramiro Jr. Franco <rjfranco@gmail.com>
2016-03-02 10:55:29 -0800

    Correcting path to json assets

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,8 +3,7 @@ import DS from 'ember-data';
 export default DS.JSONAPIAdapter.extend({
   pathForType: function(type) {
     if (window.ELECTRON) {
-      let app = require('electron').remote.app;
-      return `${app.getAppPath()}/public/${type}.json`;
+      return `${window.__dirname}/${type}.json`;
     }
 
     return `${type}.json`;
